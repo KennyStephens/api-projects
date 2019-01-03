a737aaad8d2dbfce4f8169c0dfe63a3744685986
Kenny Stephens <34525009+KennyStephens@users.noreply.github.com>
2019-01-03 10:48:09 -0700

    Finished repo project

--- 8_1_project_files/githubfinder/github.js
+++ 8_1_project_files/githubfinder/github.js
@@ -2,16 +2,22 @@ class GitHub {
     constructor() {
         this.client_id = '319a5d1e801fa3ee1d45';
         this.client_secret = '77e79eb201970c9d4af937c3589e9b6fb4d298a6';
+        this.repos_count = 5;
+        this.repos_sort = 'created: asc';
     }
 
     async getUser(user) {
         const profileResponse = await fetch(`https://api.github.com/users/${user}?client_id=${this.client_id}&client_secret=${this.client_secret}`);
 
+        const repoResponse = await fetch(`https://api.github.com/users/${user}/repos?per_page=${this.repos_count}&sort=${this.repos_sort}&client_id=${this.client_id}&client_secret=${this.client_secret}`);
+
         
         const profile = await profileResponse.json();
+        const repos = await repoResponse.json();
 
         return {
-            profile
+            profile,
+            repos
         }
     }
 }
